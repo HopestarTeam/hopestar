03d6427c17a6c2bd0a21024b1508e997b0f389fb
KickTheBoot <anoojjonttu@gmail.com>
2023-05-30 15:06:53 +0300

    make a condition that blocks clicking and hovering on clickables when the cursor is over a visual element with class GUIBackground

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,22 +1,20 @@
 using System;
-using UnityEngine.Events;
+using UnityEngine.UIElements;
+using UnityEngine;
 
 [Serializable]
 public class MenuManager
 {
-    public bool inMenu;
+    public UIDocument Hud;
+    public bool OnElement;
 
-    public UnityEvent OnMenuEnter, OnMenuExit;
-
-    public void EnterMenu()
+    public void Initialize()
     {
-        inMenu = true;
-        OnMenuEnter.Invoke();
+        Hud.rootVisualElement.Query(className: "GUIBackground").ForEach( (element) => 
+        {
+            element.RegisterCallback<MouseEnterEvent>(e => OnElement = true);
+            element.RegisterCallback<MouseLeaveEvent>(e => OnElement = false);  
+        });
     }
-
-    public void ExitMenu()
-    {
-        inMenu = false;
-        OnMenuExit.Invoke();
-    }
+    
 }
