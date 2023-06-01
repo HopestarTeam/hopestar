2d2eed1360eb8dba2fdbf36220c87199eafda1c8
nfrande <niklas.frande@gmail.com>
2023-06-01 12:48:57 +0300

    changes to endturn script

--- Assets/Scripts/EndTurn.cs
+++ Assets/Scripts/EndTurn.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class EndTurn : MonoBehaviour
 {
-   int listSize = 11; // this should be the size of the grid list
+   [SerializeField] Grid myGrid;
+  int listSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        EndTurnFunction(); //this should be called when the player pressess the End Turn Button
-        
+        listSize = myGrid.count;
+        EndTurnFunction();  //this should be called when the player pressess the End Turn Button
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       // On mouseclick, call the EndTurnFunction() 
     }
 
     void EndTurnFunction()
     {
-        for (int i =0; i < listSize; i++)
+        for (int i = 0; i < listSize; i++)
         {
             int currentItem = i;  //this should be the current item in the list
-            // Debug.Log(currentItem);
+            
         }
     }
 }
