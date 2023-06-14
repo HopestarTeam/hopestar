d3d9864cf3efa94023161164aac24243aae7c176
H8872 <mm-laine@hotmail.fi>
2023-06-14 15:33:51 +0300

    removed lenghty comments

--- Assets/Scripts/Card/CardHandler.cs
+++ Assets/Scripts/Card/CardHandler.cs
@@ -19,13 +19,6 @@ public class CardHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //cardName.text = properties.cardName;
-        //flavorText.text = properties.flavorText;
-        //if(properties.hasFunction && properties.flavorText == "")
-        //{
-        //    flavorText.text = "There should be something here";
-        //}
-
         resourceCosts = properties.resourceCosts;
         resourceUpkeep = properties.resourceUpkeep;
         resourceGains = properties.resourceGains;
@@ -35,50 +28,8 @@ public class CardHandler : MonoBehaviour
         variables = GameManager.gm.variables;
         deck = transform.parent.GetComponent<DeckBehaviour>();
 
-        //costText.text = "";
-        //upkeepText.text = "";
-        //gainsText.text = "";
-        //if(emsissionAmount > 0)
-        //    emissionText.text = emsissionAmount + "c";
-        //else
-        //    emissionText.text = "";
-        //
-        //for (int i = 0; i < resourceCosts.Length; i++)
-        //{
-        //    costText.text += resourceCosts[i].amount + resourceCosts[i].resourceType.ToString()[0].ToString() + "\n";
-        //}
-        //for (int i = 0; i < resourceUpkeep.Length; i++)
-        //{
-        //    upkeepText.text += resourceUpkeep[i].amount + resourceUpkeep[i].resourceType.ToString()[0].ToString() + "\n";
-        //}
-        //for (int i = 0; i < resourceGains.Length; i++)
-        //{
-        //    gainsText.text += resourceGains[i].amount + resourceGains[i].resourceType.ToString()[0].ToString() + " ";
-        //}
-
         mesh = transform.GetChild(0).GetComponent<MeshRenderer>();
         mesh.material.SetTexture("_MainTex",properties.cardImage.texture);
-        /*switch (properties.cardType)
-        {
-            case CardType.FOOD:
-                mesh.material.color = Color.green;
-                break;
-            case CardType.ENERGY:
-                mesh.material.color = Color.blue;
-                break;
-            case CardType.RAW:
-                mesh.material.color = Color.red;
-                break;
-            case CardType.INDUSTRY:
-                mesh.material.color = Color.gray;
-                break;
-            case CardType.URBAN:
-                mesh.material.color = Color.yellow;
-                break;
-            default:
-                mesh.material.color = Color.magenta;
-                break;
-        }*/
         
         if(properties.hasFunction)
         {
@@ -182,65 +133,6 @@ public class CardHandler : MonoBehaviour
                     break;
             }
         }
-
-        /*if(!placedThisTurn)
-        {
-            GlobalVariables variables = GameManager.gm.variables;
-            if(resourceCosts != null)
-            foreach(ResourceTypeDefinition definition in resourceCosts)
-            {
-                switch(definition.resourceType)
-                {
-                    case ResourceTypeDefinition.ResourceType.RAW:
-                        result = (variables.RawResources >= definition.amount);
-                        break;
-                    case ResourceTypeDefinition.ResourceType.FOOD:
-                        result = (variables.Food >= definition.amount);
-                        break;
-                    case ResourceTypeDefinition.ResourceType.ENERGY:
-                        result = (variables.Energy >= definition.amount);
-                        break;
-                    case ResourceTypeDefinition.ResourceType.CONSUMER:
-                        result = (variables.ConsumerGoods >= definition.amount);
-                        break;
-                    case ResourceTypeDefinition.ResourceType.INDUSTRY:
-                        result = (variables.IndustryGoods >= definition.amount);
-                        break;
-                    default:
-                        break;
-                }
-                //Debug.Log("Enough " + definition.resourceType.ToString() + " = " + result);
-                if(!result)
-                    return result;
-            }
-            if(resourceUpkeep != null)
-            foreach(ResourceTypeDefinition definition in resourceUpkeep)
-            {
-                switch(definition.resourceType)
-                {
-                    case ResourceTypeDefinition.ResourceType.RAW:
-                        result = (variables.RawResourcesUpkeep >= definition.amount);
-                        break;
-                    case ResourceTypeDefinition.ResourceType.FOOD:
-                        result = (variables.FoodUpkeep >= definition.amount);
-                        break;
-                    case ResourceTypeDefinition.ResourceType.ENERGY:
-                        result = (variables.EnergyUpkeep >= definition.amount);
-                        break;
-                    case ResourceTypeDefinition.ResourceType.CONSUMER:
-                        result = (variables.ConsumerGoodsUpkeep >= definition.amount);
-                        break;
-                    case ResourceTypeDefinition.ResourceType.INDUSTRY:
-                        result = (variables.IndustryGoodsUpkeep >= definition.amount);
-                        break;
-                    default:
-                        break;
-                }
-                //Debug.Log("Enough " + definition.resourceType.ToString() + " Upkeep = " + result);
-                if(!result)
-                    return result;
-            }
-        }*/
         return result;
     }
 
@@ -329,59 +221,6 @@ public class CardHandler : MonoBehaviour
             }
             variables.variables = tmp;
         }
-        /*
-        if(resourceCosts != null)
-        foreach(ResourceTypeDefinition definition in resourceCosts)
-        {
-            switch(definition.resourceType)
-            {
-                case ResourceTypeDefinition.ResourceType.RAW:
-                    variables.RawResources -= definition.amount;
-                    break;
-                case ResourceTypeDefinition.ResourceType.FOOD:
-                    variables.Food -= definition.amount;
-                    break;
-                case ResourceTypeDefinition.ResourceType.ENERGY:
-                    variables.Energy -= definition.amount;
-                    break;
-                case ResourceTypeDefinition.ResourceType.CONSUMER:
-                    variables.ConsumerGoods -= definition.amount;
-                    break;
-                case ResourceTypeDefinition.ResourceType.INDUSTRY:
-                    variables.IndustryGoods -= definition.amount;
-                    break;
-                default:
-                    break;
-            }
-        }
-        if(placedThisTurn)
-        {
-            if(resourceUpkeep != null)
-            foreach(ResourceTypeDefinition definition in resourceUpkeep)
-            {
-                switch(definition.resourceType)
-                {
-                    case ResourceTypeDefinition.ResourceType.RAW:
-                        variables.RawResourcesUpkeep -= definition.amount;
-                        break;
-                    case ResourceTypeDefinition.ResourceType.FOOD:
-                        variables.FoodUpkeep -= definition.amount;
-                        break;
-                    case ResourceTypeDefinition.ResourceType.ENERGY:
-                        variables.EnergyUpkeep -= definition.amount;
-                        break;
-                    case ResourceTypeDefinition.ResourceType.CONSUMER:
-                        variables.ConsumerGoodsUpkeep -= definition.amount;
-                        break;
-                    case ResourceTypeDefinition.ResourceType.INDUSTRY:
-                        variables.IndustryGoodsUpkeep -= definition.amount;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        */
     }
 
     public void ResolveCard()
