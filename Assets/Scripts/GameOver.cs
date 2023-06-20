588c7a03c61b07244c8d80518c8adc5478c7bbef
jptibirosen <11poiuy11@gmail.com>
2023-06-20 10:07:45 +0300

    some UI improvements

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -27,12 +27,12 @@ public class GameOver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        CheckGameOver();    //testing only, remove this line later
     }
 }
